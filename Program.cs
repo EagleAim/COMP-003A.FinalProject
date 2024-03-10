2daaeb4c3e52f523224090b0e238300c77f6bc5d
Logan King <logan.ramzie.king@gmail.com>
2024-03-10 15:36:05 -0700

    The base of basic information made needs to be fixed

--- Program.cs
+++ Program.cs
@@ -7,13 +7,83 @@
 
 
 
+using System.Runtime.Intrinsics.X86;
+
 namespace COMP_003A.FinalProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("".PadRight(50, '*'));
+            Console.WriteLine("Welcome to Kings County Medical");
+            Console.WriteLine("Please enter the apropiate information below to create a account");
+            Console.WriteLine("PRESS ENTER TO CONTINUTE");
+            Console.WriteLine("".PadRight(50, '*'));
+
+            ///<summary>
+            ///Basic Information Section
+            /// </summary>
+
+
+
+            //FIRST NAME AND LAST NAME
+            Console.WriteLine("PLEASE ENTER FIRST NAME");
+            string firstName = Console.ReadLine();
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("PLEASE ENTER LAST NAME");
+            string lastName = Console.ReadLine();
+            lastName = Console.ReadLine();
+
+            //AGE
+            Console.WriteLine("PLEASE ENTER YOUR BIRTHYEAR");
+            string inputAge = Console.ReadLine();
+            int yearBorn = DateTime.Now.Year - Convert.ToInt32(inputAge);
+            inputAge = Console.ReadLine();
+
+            if (yearBorn >= 1900 && yearBorn <= DateTime.Now.Year)
+            {
+
+            }
+
+            else
+            {
+                Console.WriteLine("\t Invalid Age");
+            }
+
+
+
+            //GENDER
+            Console.WriteLine("PLEASE ENTER YOUR GENDER USING M, F, OR O");
+            string inputGender = Console.ReadLine();
+            inputGender = Console.ReadLine();
+
+            if (inputGender == "M")
+            {
+                Console.WriteLine("\tMale");
+            }
+            else if (inputGender == "F")
+            {
+                Console.WriteLine("\tFemale");
+            }
+
+            else if (inputGender == "O") 
+            {
+                Console.WriteLine("\tOther not listed");
+            }
+
+            else
+            {
+                Console.WriteLine("\tInvalid Response");
+            }
+
+
             
+            Console.WriteLine($"First Name: {firstName}");
+            Console.WriteLine($"Last Name: {lastName}");
+            Console.WriteLine($"Age: {inputAge}");
+            Console.WriteLine($"Gender: {inputGender}");
         }
     }
 }
