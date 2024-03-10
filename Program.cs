a00c9d0e6a9f4c39da0dd74f937e33bccc167e3a
Logan King <logan.ramzie.king@gmail.com>
2024-03-10 15:07:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Author: Logan King
+ * course: Comp003A
+ * Purpouse: To show our skills with C# in a final project.
+*/
+
+
+
+
+namespace COMP_003A.FinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
